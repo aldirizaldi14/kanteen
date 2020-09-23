27d74896bd21384718e406e4a6a9379b4b8d64aa
Mada Baskoro <dareekun@live.com>
2020-09-24 06:08:07 +0700

    trial

--- resources/views/monitor1.blade.php
+++ resources/views/monitor1.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
         <!-- colom 1 -->
         <div class="col-sm-4">
