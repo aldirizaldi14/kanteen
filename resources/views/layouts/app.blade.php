24bdc9330f5158400c43c2322f17a0c2f1e37ddc
Mada Baskoro <dareekun@live.com>
2020-10-09 07:52:27 +0700

    remove fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,6 @@
     <script src="{{ asset('/js/app.js') }}"></script>
     <script src="{{ asset('/js/bootstrap.min.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
