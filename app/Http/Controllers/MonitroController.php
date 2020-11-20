fbd4d23db8affb1ee7979287b7dc4aa9b87593a9
Mada Baskoro <dareekun@live.com>
2020-11-20 11:40:15 +0700

    test

--- app/Http/Controllers/MonitroController.php
+++ app/Http/Controllers/MonitroController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use App\Events\BroadcastListener;
 use App\Events\AyamListener;
 use App\Events\IkanListener;
 use App\Events\DagingListener;
